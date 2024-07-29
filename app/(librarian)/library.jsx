fba6178812ffddbb2020e1c30304637075c8dc65
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2024-07-29 21:34:36 +0300

    complete librarian home page

---
+++ app/(librarian)/library.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Library = () => {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text>Library</Text>
+    </View>
+  );
+}
+
+export default Library
---
+++ app/(librarian)/write.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const WriteTags = () => {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text>Write Tags</Text>
+    </View>
+  );
+}
+
+export default WriteTags
